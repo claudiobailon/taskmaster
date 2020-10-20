93969a9b2a0cbb7777d526bc99bca3162cd4a926
claudiobailon <c.bailonschubert@gmail.com>
2020-10-20 08:32:33 -0700

    added homepage, add tasks, and all tasks and linked them

--- app/src/main/java/com/claudiobailon/taskmaster/MainActivity.java
+++ app/src/main/java/com/claudiobailon/taskmaster/MainActivity.java
@@ -2,7 +2,9 @@ package com.claudiobailon.taskmaster;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,22 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        Button goToAddTask = MainActivity.this.findViewById(R.id.addTask);
+        goToAddTask.setOnClickListener((view)-> {
+            System.out.println("Going to Add Task");
+            Intent goToAddTaskIntent = new Intent(MainActivity.this, AddTask.class);
+            MainActivity.this.startActivity(goToAddTaskIntent);
+        });
+
+        Button goToAllTasks = MainActivity.this.findViewById(R.id.allTasks);
+        goToAllTasks.setOnClickListener((view)-> {
+            System.out.println("Going to All Tasks");
+            Intent goToAllTasksIntent = new Intent(MainActivity.this, AllTasks.class);
+            MainActivity.this.startActivity(goToAllTasksIntent);
+        });
     }
+
+
 }
