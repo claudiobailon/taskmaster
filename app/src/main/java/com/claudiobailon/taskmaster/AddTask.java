7678a68910d6d77a4fdeba0c08b7502d34ae5ba4
claudiobailon <c.bailonschubert@gmail.com>
2020-10-20 15:27:45 -0700

    Created back button and display message when task is added

--- app/src/main/java/com/claudiobailon/taskmaster/AddTask.java
+++ app/src/main/java/com/claudiobailon/taskmaster/AddTask.java
@@ -2,7 +2,11 @@ package com.claudiobailon.taskmaster;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class AddTask extends AppCompatActivity {
 
@@ -10,5 +14,21 @@ public class AddTask extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_task);
+
+
+        Button addButton = AddTask.this.findViewById(R.id.addTaskNow);
+        addButton.setOnClickListener((view) -> {
+
+            Toast.makeText(getApplicationContext(), "Task Added!", Toast.LENGTH_LONG).show();
+
+        });
+
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        Intent intent = new Intent(AddTask.this, MainActivity.class);
+        AddTask.this.startActivity(intent);
+        return true;
     }
 }
--- app/src/main/java/com/claudiobailon/taskmaster/AllTasks.java
+++ app/src/main/java/com/claudiobailon/taskmaster/AllTasks.java
@@ -2,7 +2,9 @@ package com.claudiobailon.taskmaster;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
 
 public class AllTasks extends AppCompatActivity {
 
@@ -10,5 +12,14 @@ public class AllTasks extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_all_tasks);
+
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
     }
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        Intent intent = new Intent(AllTasks.this, MainActivity.class);
+        AllTasks.this.startActivity(intent);
+        return true;
+    }
+
 }
