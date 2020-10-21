ec507e4466665ec99788e17a6be0b81b9ef037d7
claudiobailon <c.bailonschubert@gmail.com>
2020-10-20 21:32:15 -0700

    All features work

--- README.md
+++ README.md
@@ -1,8 +1,13 @@
 # taskmaster
 An app to master your tasks!
+## App Screenshots
+![Homepage](screenshots/27home.png) 
+![All Tasks](screenshots/taskDetails.png)
 ## Change Log
 ### 10-19-2020
 Created app and added activities for home, add task, and all tasks.
-![Homepage](screenshots/homepage.png)
-![Add Tasks](screenshots/addTasks.png)
-![All Tasks](screenshots/allTasks.png)
+### 10-20-2020
+Added three task buttons to home page that link to a task details page.  The task details page displays
+the name of the task of the button that you clicked and also allows you to navigate back home.  A settings
+page was also added with a link to it on the home page.  In the setting page, you can set a username that will
+then display on the homepage. 
---
+++ app/src/main/java/com/claudiobailon/taskmaster/TaskDetail.java
@@ -0,0 +1,31 @@
+package com.claudiobailon.taskmaster;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.MenuItem;
+import android.widget.TextView;
+
+public class TaskDetail extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_task_detail);
+
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        Intent intent = getIntent();
+        TextView taskName = findViewById(R.id.taskName);
+        taskName.setText(intent.getExtras().getString("task"));
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        Intent intent = new Intent(TaskDetail.this, MainActivity.class);
+        TaskDetail.this.startActivity(intent);
+        return true;
+    }
+
+}
